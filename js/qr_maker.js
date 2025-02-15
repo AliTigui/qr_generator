67cfcefb70c9ea166c8eb0caa2b4df084bcbcd10
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2025-02-15 13:49:58 +0100

    Update qr_maker.js

--- js/qr_maker.js
+++ js/qr_maker.js
@@ -2,7 +2,28 @@ var qrcode = new QRCode(document.getElementById("qrcode"), {
     width : 350,
     height : 350
 });
+function base64ToBlob(base64, contentType = "",
+    sliceSize = 512) {
+    const byteCharacters = atob(base64.split(",")[1]);
+    const byteArrays = [];
 
+    for (let offset = 0; offset < byteCharacters.length;
+        offset += sliceSize) {
+        const slice = byteCharacters.slice(
+            offset, offset + sliceSize);
+
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+}
 function makeCode () {		
     let family_name = document.getElementById("family_name").value;
     let name = document.getElementById("name").value;  
@@ -20,18 +41,8 @@ function download () {
     let img = document.querySelector("img");
     let src = document.querySelector("img").src;
     let link = document.createElement("a");
-            const base64Data = src.replace(/^data:.+;base64,/, '');
-            const byteCharacters = atob(base64Data); 
-            const byteNumbers = new Array(byteCharacters.length);
-
-            for (let i = 0; i < byteCharacters.length; i++) {
-                byteNumbers[i] = byteCharacters.charCodeAt(i);
-            }
-
-            const byteArray = new Uint8Array(byteNumbers);
-            const blob = new Blob([byteArray], { type: "image/png" });
-            const url = URL.createObjectURL(blob);
-
+    let blob = base64ToBlob(src,"image/png")
+    let url = URL.createObjectURL(blob);
     link.href=url;
     link.download="qr.png";
     link.appendChild(img);
