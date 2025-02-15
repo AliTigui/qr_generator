d4372c55932b430708883258eeeb878d1923b486
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2025-02-15 13:04:55 +0100

    Update qr_maker.js

--- js/qr_maker.js
+++ js/qr_maker.js
@@ -20,12 +20,24 @@ function download () {
     let img = document.querySelector("img");
     let src = document.querySelector("img").src;
     let link = document.createElement("a");
-    link.href=src;
+            const base64Data = src.replace(/^data:.+;base64,/, '');
+            const byteCharacters = atob(base64Data); 
+            const byteNumbers = new Array(byteCharacters.length);
+
+            for (let i = 0; i < byteCharacters.length; i++) {
+                byteNumbers[i] = byteCharacters.charCodeAt(i);
+            }
+
+            const byteArray = new Uint8Array(byteNumbers);
+            const blob = new Blob([byteArray], { type: "image/png" });
+            const url = URL.createObjectURL(blob);
+
+    link.href=url;
     link.download="qr.png";
     link.appendChild(img);
     document.querySelector("#qrcode").appendChild(link);
     link.click();
-
+URL.revokeObjectURL(url);
 }
 
 $("#download").
